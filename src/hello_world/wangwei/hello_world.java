92f74bed6e3f63f33e4a2f2996d1f1a4b5fb2403
thinkerww <manwwhello@163.com>
2021-09-02 12:24:14 +0800

    furst

---
+++ src/hello_world/wangwei/hello_world.java
@@ -0,0 +1,9 @@
+package hello_world.wangwei;
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello_World");
+        System.out.println("Hello_World");
+
+    }
+}
